aa2451fb9d9776c783a2faeb7064437d529e4f45
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-10-05 22:58:02 -0300

    Genero url desde la clase Trip

--- Trip.rb
+++ Trip.rb
@@ -1,5 +1,6 @@
 class Trip
     attr_accessor :base_url
+    attr_reader :response
 
     def initialize(city_code = nil, start_date = nil, end_date = nil)
         @city_code = city_code
@@ -10,5 +11,15 @@ class Trip
 
     def to_s
         "City code: #{@city_code} | from: #{@start_date} to #{@end_date}"
+    end
+
+    def getURL
+        "#{@base_url}#{@city_code}/#{@start_date.year}-#{@start_date.month}-#{@start_date.day}/#{@end_date.year}-#{@end_date.month}-#{@end_date.day}/1/0/0/TOTALFARE/ASCENDING/NA/NA/NA/NA/NA"
+    end
+
+    def getData
+        uri = URI(self.getURL)
+        response = Net::HTTP.get(uri)
+        @response = JSON.parse(response)
     end
 end
--- get_trip_prices.rb
+++ get_trip_prices.rb
@@ -33,38 +33,33 @@ while to <= end_date
     to_aux = (to-one_day*margin)
     threads = []
     while to_aux <= (to+one_day*margin)
-        trip = Trip.new('MIA', from, to_aux)
-        puts trip.base_url
-        trip.base_url = 'http://www.despegar.cl/shop/flights/data/search/roundtrip/scl/'
-        puts trip.base_url
-        # threads.push(Thread.new(to_aux){ |to_aux|
-        #     url = "http://www.despegar.cl/shop/flights/data/search/roundtrip/scl/#{options['city']}/#{from.year}-#{from.month}-#{from.day}/#{to_aux.year}-#{to_aux.month}-#{to_aux.day}/1/0/0/TOTALFARE/ASCENDING/NA/NA/NA/NA/NA"
-        #     uri = URI(url)
-        #     response = Net::HTTP.get(uri)
-        #     json = JSON.parse(response)
-        #     price = json["result"]["data"]["items"].first["emissionPrice"]["total"]["fare"]["raw"]
-        #     price_output = price.formatWithPoints
-        #
-        #     if price >= options['price_ranges']['excessive']
-        #         price_output = price_output.red
-        #     elsif price >= options['price_ranges']['expensive']
-        #         price_output = price_output.light_red
-        #     elsif price >= options['price_ranges']['moderate']
-        #         price_output = price_output.yellow
-        #     elsif price >= options['price_ranges']['moderate_to_cheap']
-        #         price_output = price_output.light_yellow
-        #     elsif price >= options['price_ranges']['cheap']
-        #         price_output = price_output.green
-        #     else
-        #         price_output = price_output.light_green
-        #     end
-        #
-        #     puts "#{from.day}/#{from.month} to #{to_aux.day}/#{to_aux.month}: #{price_output}"
-        #
-        #     File.open(output, 'a') {|file|
-        #         file.write "#{from.day};#{from.month};#{from.year};#{to_aux.day};#{to_aux.month};#{to_aux.year};" + json["result"]["data"]["items"].first["emissionPrice"]["total"]["fare"]["raw"].to_s + "\n"
-        #     }
-        # })
+        threads.push(Thread.new(to_aux){ |to_aux|
+            trip = Trip.new('MIA', from, to_aux)
+            trip.base_url = 'http://www.despegar.cl/shop/flights/data/search/roundtrip/scl/'
+            json = trip.getData
+            price = json["result"]["data"]["items"].first["emissionPrice"]["total"]["fare"]["raw"]
+            price_output = price.formatWithPoints
+
+            if price >= options['price_ranges']['excessive']
+                price_output = price_output.red
+            elsif price >= options['price_ranges']['expensive']
+                price_output = price_output.light_red
+            elsif price >= options['price_ranges']['moderate']
+                price_output = price_output.yellow
+            elsif price >= options['price_ranges']['moderate_to_cheap']
+                price_output = price_output.light_yellow
+            elsif price >= options['price_ranges']['cheap']
+                price_output = price_output.green
+            else
+                price_output = price_output.light_green
+            end
+
+            puts "#{from.day}/#{from.month} to #{to_aux.day}/#{to_aux.month}: #{price_output}"
+
+            File.open(output, 'a') {|file|
+                file.write "#{from.day};#{from.month};#{from.year};#{to_aux.day};#{to_aux.month};#{to_aux.year};" + json["result"]["data"]["items"].first["emissionPrice"]["total"]["fare"]["raw"].to_s + "\n"
+            }
+        })
 
         to_aux += one_day
     end
