bada26899ccd68438b611db0353c24b6468d667a
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-10-07 11:59:03 -0300

    Añadi soporte para threads

--- get_trip_prices.rb
+++ get_trip_prices.rb
@@ -17,6 +17,7 @@ OptionParser.new do |opts|
   opts.on("-t", "--destination-city destination-city", "Destination city code") { |v| options[:destination_city] = v.to_s.upcase }
   opts.on("-d", "--duration days", "Duration in days") { | v |  options[:duration_in_days] = v.to_i }
   opts.on("-m", "--margin margin-in-days", "Margin in days") { | v |  options['margin'] = v.to_i }
+  opts.on("-n", "--threads number-of-threads", "Number of threads") { | v |  options['number_of_threads'] = v.to_i }
   opts.on("--debug", "--debug", "Activate debug") { | v |  options[:debug] = true }
 end.parse!
 
@@ -32,33 +33,40 @@ logger.warn("Using debug mode") if options[:debug]
 logger.info("Origin city code: #{options[:origin_city].upcase}")
 logger.info("Destination city code: #{options[:destination_city].upcase}")
 
-from     = Time.utc(2016,10,6)
-end_date = Time.utc(2016,12,31)
+from     = Time.utc(2016,12,31)
+end_date = Time.utc(2017,07,31)
 to       = from + options[:duration_in_days]
 
 output = "results/#{options[:origin_city]}_#{options[:destination_city]}_#{from.strftime('%d_%b_%Y')}_to_#{end_date.strftime('%d_%b_%Y')}_#{Time.now.to_i}.csv".downcase
-File.open(output, 'w') { |file| file.write('from day;from month;from year;to day;to month;to year;cost') }
+File.open(output, 'w') { |file| file.write("From day;From month;From year;To day;To month;To year;Price\n") }
 
 date_format = '%d %b'
 from = from.yesterday
-trip = Trip.new(options[:origin_city], options[:destination_city])
-trip.base_url = 'http://www.despegar.cl/shop/flights/data/search/roundtrip/'
-trip.debug = true if options[:debug]
-trip.setRanges(options['price_ranges'])
+threads = []
 while to <= end_date
     from = from.tomorrow
     to   = from.addDays(options[:duration_in_days])
     puts "From: ".blue + "#{from.strftime(date_format)}".light_blue
     (-options['margin']..options['margin']).each do | margin |
         current_to = to.addDays(margin)
-        trip.start_date = from
-        trip.end_date = current_to
-        json = trip.getData
-        price = trip.getLowestPrice.formatWithPoints
-        puts "\t- To ".blue + "#{current_to.strftime(date_format)}:".light_blue + " #{trip.getLowestPriceWithColor}"
-        File.open(output, 'a') {|file|
-            file.write "#{trip.start_date.day};#{trip.start_date.month};#{trip.start_date.year};#{trip.end_date.day};#{trip.end_date.month};#{trip.end_date.year};" + price + "\n"
+        threads.push << Thread.new(from, current_to){ | from, to |
+            trip = Trip.new(options[:origin_city], options[:destination_city])
+            trip.base_url = 'http://www.despegar.cl/shop/flights/data/search/roundtrip/'
+            trip.debug = true if options[:debug]
+            trip.setRanges(options['price_ranges'])
+            trip.start_date = from
+            trip.end_date = current_to
+            json = trip.getData
+            price = trip.getLowestPrice.formatWithPoints
+            puts "\t- To ".blue + "#{current_to.strftime(date_format)}:".light_blue + " #{trip.getLowestPriceWithColor}"
+            File.open(output, 'a') {|file|
+                file.write "#{trip.start_date.day};#{trip.start_date.month};#{trip.start_date.year};#{trip.end_date.day};#{trip.end_date.month};#{trip.end_date.year};" + price + "\n"
+            }
         }
+        threads.each { | thread | thread.join } and threads = [] if threads.count == options['number_of_threads']
     end
+    threads.each { | thread | thread.join }
+    threads = []
     puts
 end
+threads.each { | thread | thread.join }
